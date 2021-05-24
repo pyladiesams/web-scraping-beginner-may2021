dbac6163fbdc74f0f89ec39ca083b8d646054db4
danielamiranda <danymiranda9030@gmail.com>
2021-05-25 00:51:11 +0200

    Updating Azure function, only wordcloud library issue to be solved

--- solutions/wordcloud_azure_function/BlobTriggerWordcloud/__init__.py
+++ solutions/wordcloud_azure_function/BlobTriggerWordcloud/__init__.py
@@ -1,9 +1,23 @@
 import logging
 
 import azure.functions as func
+import wordcloud
+
+# from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 
-def main(inputBlob: func.InputStream, outputBlob: func.Out):
+def main(myBlob: func.InputStream, inputBlob: bytes, outputBlob: func.Out[bytes]):
     logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {inputBlob.name}\n"
-                 f"Blob Size: {inputBlob.length} bytes")
+                 f"Name: {myBlob.name}\n"
+                 f"Blob Size: {myBlob.name} bytes")
+
+    # Reading input blob and Converting list to plain text
+    logging.info("Getting input file as a single string")
+    recipe_names_text = inputBlob.replace("\n", " ").replace('\r', '')
+
+    # Create and generate a word cloud image:
+    word_cloud = wordcloud.WordCloud(background_color="white").generate(recipe_names_text)
+
+    # Save wordcloud in output file
+    outputBlob.set(wordcloud.to_file(f"{myBlob.name}.png"))
+    # outputBlob.set(recipe_names_text)
